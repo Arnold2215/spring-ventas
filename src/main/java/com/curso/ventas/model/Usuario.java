e24dc4451016eb3e8c2d92e067d608faff5a1408
ARNOLD2215 <74846123@certus.edu.pe>
2023-05-29 22:52:38 -0500

    ventas

---
+++ src/main/java/com/curso/ventas/model/Usuario.java
@@ -0,0 +1,5 @@
+package com.curso.ventas.model;
+
+public class Usuario {
+
+}
---
+++ src/test/java/com/curso/ventas/SpringVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.ventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
